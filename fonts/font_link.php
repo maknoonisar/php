5ffd2aed4e5832901b4692d7e33a5565bbdff8b9
Maknoon Nisar <111339001+maknoonisar@users.noreply.github.com>
2022-11-24 15:09:22 +0500

    Add files via upload

---
+++ fonts/font_link.php
@@ -0,0 +1,15 @@
+
+
+
+	<!--font link-------------------->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	
+	<!--ubuntu -->
+	<link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+
+	<!--heebo-->
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Heebo&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
+	
